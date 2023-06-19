8cc69b51113e60c8bf132545bb00f35f6135483c
gregorywams <wamunyimagregory2@gmail.com>
2023-06-19 11:30:20 -0700

    print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,21 +14,19 @@ int main(void)
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
+	
+			for (l = 50; l < 58; l++)
 			{
-				for (l = 50; l < 58; l++)
+				if (l > m && m > n)
 				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
 
-					if (l > m && m > n)
+					if (n != 55 || m != 56)
 					{
-						putchar(n);
-						putchar(m);
-						putchar(l);
-
-						if (n != 55 || m != 56)
-						{
-						putchar(',');
-						putchar(' ');
-						}
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
