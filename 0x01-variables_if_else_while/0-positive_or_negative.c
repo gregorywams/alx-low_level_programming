1f471f72050d74ec6c513b1bf34b7635f9d04c4c
gregorywams <wamunyimagregory2@gmail.com>
2023-06-18 17:19:40 -0700

    0, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,7 @@
  */
 
 int main(void)
-
 {
-
 	int n;
 
 	srand(time(0));
@@ -22,12 +20,12 @@ int main(void)
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
-	}	
-	else if (n < 0)
+	}
+	if (n < 0)
 	{
 	printf("%d is negative\n", n);
 	}
-	else (n == 0)
+	if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
