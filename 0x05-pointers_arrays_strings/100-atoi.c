2f860bc9eecabbe16d9391f239e77218c789aee6
gregorywams <wamunyimagregory2@gmail.com>
2023-06-27 09:32:00 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,40 +7,21 @@
  * Return: the int converted from the string
  */
 
-int _atoi(char *s)
+int main(void)
 {
-	int i, d, n, len, f, digit;
+	int r = 0, c = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	time_t t;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	srand((unsigned int) time(&t));
+	while (c < 2772)
 	{
-	if (s[i] == '-')
-		++d;
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-		digit = s[i] - '0';
-	if (d % 2){
-		digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
 	}
-	if (s[i + 1] < '0' || s[i + 1] > '9'){
-		break;
-		f = 0;
-	}
-	}
-		i++;
-	}
-	if (f == 0)
-
+	printf("%c\n", (2772 - c));
 	return (0);
-	return (n);
 }
